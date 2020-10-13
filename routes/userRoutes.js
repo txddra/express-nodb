21a0db7ff7fb20abe908449326386689c969cd80
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-13 14:20:45 -0400

    update route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,45 +8,9 @@ router.get('/all-users',getAllUsers);
 
 //get one user based on id 
 const{createNewUser} = require('../controllers/userControllers.js')
-router.get('/single-user/:id',(req, res)=>{
-let foundUser =  users.filter((user)=>{
-    if(user.id === req.params.id){
-    return res.json({confirmation: 'success', user})
-    }
-  })
-  if(!foundUser.length){
-  return res.status(400).json({confirmation: 'fail', message: 'User Does Not Exist'})}
-});
+router.get('/single-user/:id',createNewUser);
 
 
-//when posting, whatever is put insdie the input is put inside the body
-router.post('/create-user',(req, res)=>{
-//error first handling/validate inputs
-if(!req.body.name || !req.body.email || !req.body.password){
-    return res.status(400).json({conformation:'fail', message:'All inputs must be filled'})
-}   
-   
-let existingUser= users.filter((user)=> user.email===req.body.email);
-if(existingUser.length){
-        return res.status(400).send('User already Exists')
-    }
-
-
-
-    //creating an object
-const newUser ={};
-//values for newUser based on req.body inputs in postman 
-newUser.name = req.body.name;
-newUser.email= req.body.email;
-newUser.password = req.body.password;
-newUser.id= String(users.length + 1);
-
-//add the user to the array
-users.push(newUser);
-//return to the user
-return res.status(200).json({confirmation: 'Success',newUser})
-// res.send(newUser)
-});
 
 router.put('/update-user/:id',(req, res)=>{
 let updateUser = req.body;
